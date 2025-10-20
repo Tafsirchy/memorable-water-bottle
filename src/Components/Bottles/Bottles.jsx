5ea38de5be3a0d2eb5efe9a8f9374f6e42b9cfcf
Tafsirchy <tafsirchy1000@gmail.com>
2025-10-20 21:40:29 +0600

    Create Shopping Cart to handle Event with parameters

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -3,18 +3,21 @@ import Bottle from '../Bottle/Bottle';
 import './Bottles.css';
 
 const Bottles = ({bottlesPromise}) => {
-    // const [cart, setCart] = useState
+    const [cart, setCart] = useState([]);
 
-    const bottles = use (bottlesPromise);
+    const bottles = use(bottlesPromise);
 
-    const handleAddToCart = () => {
-        alert('Added to cart');
+    const handleAddToCart = (bottle) => {
+        // alert('Added to cart');
+        const newCart = [...cart, bottle];
+        setCart(newCart);
     }
 
     console.log(bottles);
     return (
         <div>
             <h3>Bottles: {bottles.length} </h3>
+            <p>Added to cart: {cart.length}</p>
             <div className='bottles-container'>
                 {
                     bottles.map(bottle => <Bottle handleAddToCart={handleAddToCart} key={bottle.id} bottle={bottle}></Bottle>)
