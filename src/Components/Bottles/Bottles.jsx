61db0fd5cb06d215883d6fd1c40af3a522edf621
Tafsirchy <tafsirchy1000@gmail.com>
2025-10-20 23:10:57 +0600

    use effect dependency and load stored id

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,16 +1,40 @@
-import React, { use, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import Bottle from '../Bottle/Bottle';
 import './Bottles.css';
+import { addToStoreCart, getStoreCart } from '../../Utilities/localStorage';
 
 const Bottles = ({bottlesPromise}) => {
     const [cart, setCart] = useState([]);
 
     const bottles = use(bottlesPromise);
 
+    // useEffect to load the cart from local storage
+    useEffect(() => {
+        const storedCartIds = getStoreCart();
+        // console.log(storedCartIds, bottles);
+
+        const storedCart = [];
+
+        for(const id of storedCartIds){
+            // console.log(id);
+            const cartBottle = bottles.find(bottle => bottle.id === id);
+            if(cartBottle){
+                storedCart.push(cartBottle);
+            }
+        }
+
+        console.log(storedCart);
+        setCart(storedCart);
+
+    }, [bottles]);
+
     const handleAddToCart = (bottle) => {
         // alert('Added to cart');
         const newCart = [...cart, bottle];
         setCart(newCart);
+
+        // save the bottle id in the storage
+        addToStoreCart(bottle.id);
     }
 
     console.log(bottles);
---
+++ src/Utilities/localStorage.js
@@ -0,0 +1,34 @@
+/**
+ * 1. To get something from local storage you will get it as a string
+ * 2. Convert this to javascript object/array
+ * 
+ */
+
+//getting data from local storage
+const getCartFromLocalStorage = () => {
+    const storedCartString = localStorage.getItem('cart');
+
+    if(storedCartString) {
+        const storedCart = JSON.parse(storedCartString);
+        return storedCart;
+    }
+    return [];
+}
+
+//saving data to local storage
+const saveCartToLocalStorage = (cart) => {
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart',cartStringified)
+}
+
+//adding item to local storage
+const addItemToLocalStorage = (id) => {
+    const cart = getCartFromLocalStorage();
+    const newCart = [...cart, id];
+    // cart.push(id);
+
+    // save new cart to local storage
+    saveCartToLocalStorage(newCart);
+}
+
+export { getCartFromLocalStorage as getStoreCart, addItemToLocalStorage as addToStoreCart}
